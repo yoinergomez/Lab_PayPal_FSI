979a3c2dd41d5427205a7e4f1817e50cee6814ce
yoiner.gomez <yoiner.gomez22@gmail.com>
2016-11-04 10:55:08 -0500

    Creando POJO item

---
+++ src/java/co/edu/udea/entity/Item.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.udea.entity;
+
+/**
+ *
+ * @author Yoiner Esteban Gomez Ayala yoiner.gomez22@gmail.com
+ */
+public class Item {
+    
+}
