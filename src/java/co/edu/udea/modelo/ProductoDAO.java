2d9dc3c1481ecf5350fba277542e7f6ee2c8181c
yoiner.gomez <yoiner.gomez22@gmail.com>
2016-11-04 11:05:52 -0500

    Agregando metodo de obtener todos los productos en el DAO

--- src/java/co/edu/udea/modelo/ProductoDAO.java
+++ src/java/co/edu/udea/modelo/ProductoDAO.java
@@ -5,10 +5,32 @@
  */
 package co.edu.udea.modelo;
 
+import co.edu.udea.entity.Producto;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
 /**
  *
  * @author Yoiner Esteban Gomez Ayala yoiner.gomez22@gmail.com
  */
 public class ProductoDAO {
     
+    public List<Producto> getAll(){
+        Session session = null;
+        List<Producto> lista = null;
+        try{
+            session = HibernateUtil.getSessionFactory().openSession();
+            Query query = session.createQuery("from Producto");
+            lista = (List<Producto>) query.list();
+        } catch(HibernateException e){
+            System.out.println(e.getMessage());
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return lista;
+    }
 }
